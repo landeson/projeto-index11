9ac931f745c425e4e34620cb5536a093c1581567
Landeson <landeson27@gmail.com>
2023-06-19 21:28:34 -0300

    projeto-index-11

---
+++ script.js
@@ -0,0 +1,99 @@
+function cliqueiNoSubmit() {
+
+    const submit = document.getElementById("submit")
+    
+
+   
+    submit.style.background = "#FFFFFF"
+    submit.style.color = "#FC7614"
+    
+
+    console.log(submit.innerHTML)
+    
+
+}
+
+function cliqueiNoSubmit() {
+
+   const obrigado = document.getElementById("Obrigado")
+
+   obrigado.style.display = "block"
+   
+   console.log(obrigado)
+     
+
+   
+    
+
+    
+    
+
+}
+
+
+
+function bolinha() {
+
+    const bolinha = document.getElementById("bolinha1")
+
+
+    bolinha.style.background = "#FC7614"
+    bolinha.style.color = "#ffffff"
+
+    console.log(bolinha)
+
+}
+
+function bolinha2() {
+
+    const bolinha = document.getElementById("bolinha2")
+
+
+    bolinha.style.background = "#FC7614"
+    bolinha.style.color = "#ffffff"
+
+    console.log(bolinha)
+
+}
+
+function bolinha3() {
+
+    const bolinha = document.getElementById("bolinha3")
+
+
+    bolinha.style.background = "#FC7614"
+    bolinha.style.color = "#ffffff"
+
+    console.log(bolinha)
+
+}
+
+function bolinha4() {
+
+    const bolinha = document.getElementById("bolinha4")
+
+
+
+    bolinha.style.background = "#FC7614"
+    bolinha.style.color = "#ffffff"
+
+
+    console.log(bolinha)
+
+}
+
+function bolinha5() {
+
+    const bolinha = document.getElementById("bolinha5")
+
+
+    bolinha.style.background = "#FC7614"
+    bolinha.style.color = "#ffffff"
+
+    console.log(bolinha)
+
+}
+
+
+
+
